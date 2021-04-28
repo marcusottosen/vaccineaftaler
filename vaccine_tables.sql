3df0b1fdd9f37ca54af8965191ce62faf010fd81
Marcus <mkottosen@gmail.com>
2021-04-28 14:12:27 +0200

    Occurs implemented

--- vaccine_tables.sql
+++ vaccine_tables.sql
@@ -1,35 +1,49 @@
-DROP TABLE IF EXISTS Person;
-DROP TABLE IF EXISTS Customer;
+#DROP DATABASE vaccineberedskabet;
+#CREATE DATABASE vaccineberedskabet;
+#USE vaccineberedskabet;
+
 DROP TABLE IF EXISTS Certificate;
 DROP TABLE IF EXISTS Stock;
 DROP TABLE IF EXISTS emp_dept;
+DROP TABLE IF EXISTS Appointment;
+DROP TABLE IF EXISTS Occurs;
+DROP TABLE IF EXISTS Customer;
 DROP TABLE IF EXISTS Department;
-DROP TABLE IF EXISTS Appointments;
 DROP TABLE IF EXISTS Employee;
 DROP TABLE IF EXISTS Vaccinetype;
 
 CREATE TABLE Employee (
-    emp_no      SMALLINT auto_increment,
-    emp_CPR     BIGINT(11),
-    emp_name	TEXT(50),
-    title		ENUM('Manager', 'Nurse', 'Doctor'),
-    hours		TINYINT,
-    salary      DECIMAL(5,2),
-    email		TEXT(30),
-    phone		INT(8),
-    city		VARCHAR(20),
-    street		VARCHAR(20),
-    house_no	VARCHAR(5),
-    address		VARCHAR(50) GENERATED ALWAYS AS (CONCAT(city, ' ', street, ' ', house_no)) VIRTUAL,
-    #date_of_birth DATE,
+    emp_no          SMALLINT auto_increment,
+    emp_CPR         BIGINT(11),
+    emp_name	    TEXT(50),
+    title		    ENUM('Manager', 'Nurse', 'Doctor'),
+    hours		    TINYINT,
+    salary          DECIMAL(5,2),
+    email		    TEXT(30),
+    phone		    INT(8),
+    city		    VARCHAR(20),
+    street		    VARCHAR(20),
+    house_no	    SMALLINT,
+    address		    VARCHAR(50) GENERATED ALWAYS AS (CONCAT(city, ' ', street, ' ', house_no)) VIRTUAL,
+    date_of_birth   DATE,
     PRIMARY KEY (emp_no)
 );
 
-create table Department(
-	dept_no 	SMALLINT auto_increment,
-	dept_name 	VARCHAR (25),
-	city 		VARCHAR (15),
-PRIMARY KEY (dept_no)
+create TABLE Department(
+	dept_no 	    SMALLINT auto_increment,
+	dept_name 	    VARCHAR (25),
+	city		    VARCHAR(20),
+    street		    VARCHAR(20),
+    house_no	    SMALLINT,
+	dept_address    VARCHAR(50) GENERATED ALWAYS AS (CONCAT(city, ' ', street, ' ', house_no)) VIRTUAL,
+PRIMARY KEY (dept_no, city)
+);
+
+CREATE TABLE Occurs (
+	city		VARCHAR(20),
+    dept_no		SMALLINT,
+    PRIMARY KEY (city),
+    FOREIGN KEY (dept_no) REFERENCES Department (dept_no)
 );
 
 CREATE TABLE emp_dept (
@@ -59,27 +73,14 @@ FOREIGN KEY (vaccine_type)
 	REFERENCES Vaccinetype(vaccine_type)
 );
 
-CREATE TABLE Appointments (
-	daily_appointment_ID	DATE,
-	CPR				        BIGINT(11),
-	customer_name			TEXT(30),
-	appointment_date		DATE,
-	appointment_time	    DATE,
-	vaccine_type	        ENUM('covaxx','aspera','blast3000','divoc'),
-	city			        VARCHAR(20),
-PRIMARY KEY(CPR, daily_appointment_ID),
-FOREIGN KEY (vaccine_type) 
-	REFERENCES Vaccinetype(vaccine_type)
-);
-
 CREATE TABLE Customer (
 	CPR				BIGINT(11),
-	emp_no			SMALLINT auto_increment,
-PRIMARY KEY (CPR),
-FOREIGN KEY (emp_no) 
-	REFERENCES Employee(emp_no),
-FOREIGN KEY (CPR)
-	REFERENCES Appointments(CPR)
+	customer_name	TEXT(30),
+	city		    VARCHAR(20),
+    street		    VARCHAR(20),
+    house_no	    SMALLINT,
+    address		    VARCHAR(50) GENERATED ALWAYS AS (CONCAT(city, ' ', street, ' ', house_no)) VIRTUAL,
+PRIMARY KEY (CPR)
 );
 
 create table Certificate(
@@ -92,9 +93,25 @@ FOREIGN KEY (emp_no) REFERENCES Employee (emp_no),
 FOREIGN KEY (vaccine_type) REFERENCES Vaccinetype(vaccine_type)
 );
 
+CREATE TABLE Appointment (
+	daily_appointment_ID	SMALLINT auto_increment,
+	CPR				        BIGINT(11),
+	appointment_name		TEXT(30),
+	appointment_date		DATE,
+	appointment_time	    TIME,
+	vaccine_type	        ENUM('covaxx','aspera','blast3000','divoc'),
+	emp_no                  SMALLINT,
+	city		    	VARCHAR(20),
+PRIMARY KEY(daily_appointment_ID),
+FOREIGN KEY (CPR) REFERENCES Customer(CPR),
+FOREIGN KEY (vaccine_type) REFERENCES Vaccinetype(vaccine_type),
+FOREIGN KEY (emp_no) REFERENCES Employee(emp_no),
+FOREIGN KEY (city) REFERENCES Occurs(city)
+);
+
 INSERT Employee VALUES
-(NULL, 2447965342, 'marcus thomsen', 'Manager', 100, 210, 'marcus323@gmail.com' , 34256236, 'København', 'tøjmestervej', 16, NULL);
-#(NULL, 4363456346, 160, 'Nurse', 'thomas3523@gmail.com' , 54457565, 'Overhærupvej 128, 4951 Nørreballe'),
+(NULL, 2447965342, 'marcus thomsen', 'Manager', 100, 210, 'marcus323@gmail.com' , 34256236, 'København', 'tøjmestervej', 16, NULL, '1999-04-26'),
+(NULL, 4363456346, 'Thomas Marcussen', 'Nurse',90, 160, 'thomas3523@gmail.com' , 54457565, 'Nørreballe', 'Overhærupvej', 128, NULL, '1901-04-26');
 #(NULL, 5647376536, 210, 'Doctor', 'tim352@gmail.com' , 27542754, 'Frørupvej 99, 1149 KøbenhavnK'),
 #(NULL, 3637245453, 210, 'Doctor', 'bob123@gmail.com' , 56742234, 'Åledalen 35, 1902 Frederiksberg C'),
 #(NULL, 5634876385, 200, 'Doctor', 'tim392@gmail.com' , 78987597, 'Christianslundsvej 50, 4532 Gislinge'),
