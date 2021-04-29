990d2705fe3e965b10cb317241ba4d245ed9861e
Victor E S Kongsbak <54668748+Kongsbak99@users.noreply.github.com>
2021-04-29 10:13:10 +0200

    vaccination added

--- vaccine_tables.sql
+++ vaccine_tables.sql
@@ -100,22 +100,30 @@ CREATE TABLE Appointment (
 	appointment_date		DATE,
 	appointment_time	    TIME,
 	vaccine_type	        ENUM('covaxx','aspera','blast3000','divoc'),
-	emp_no                  SMALLINT,
 	city		    	VARCHAR(20),
 PRIMARY KEY(daily_appointment_ID),
 FOREIGN KEY (CPR) REFERENCES Customer(CPR),
 FOREIGN KEY (vaccine_type) REFERENCES Vaccinetype(vaccine_type),
-FOREIGN KEY (emp_no) REFERENCES Employee(emp_no),
 FOREIGN KEY (city) REFERENCES Occurs(city)
 );
 
-INSERT Employee VALUES
-(NULL, 2447965342, 'marcus thomsen', 'Manager', 100, 210, 'marcus323@gmail.com' , 34256236, 'København', 'tøjmestervej', 16, NULL, '1999-04-26'),
-(NULL, 4363456346, 'Thomas Marcussen', 'Nurse',90, 160, 'thomas3523@gmail.com' , 54457565, 'Nørreballe', 'Overhærupvej', 128, NULL, '1901-04-26');
-#(NULL, 5647376536, 210, 'Doctor', 'tim352@gmail.com' , 27542754, 'Frørupvej 99, 1149 KøbenhavnK'),
-#(NULL, 3637245453, 210, 'Doctor', 'bob123@gmail.com' , 56742234, 'Åledalen 35, 1902 Frederiksberg C'),
-#(NULL, 5634876385, 200, 'Doctor', 'tim392@gmail.com' , 78987597, 'Christianslundsvej 50, 4532 Gislinge'),
-#(NULL, 4457374565, 190, 'Doctor', 'thomas.thimsen@gmail.com' , 87957857, 'Blæsenborgvej 5, 1064 Svenderup '),
-#(NULL, 5747356365, 200, 'Doctor', 'ole.wedel@gmail.com' , 69554766, 'Sibiriensvej 28, 4243 Rude'),
-#(NULL, 6574856856, 210, 'Doctor', 'nick420@gmail.com' , 34527676, 'Ladbyvej 89, 6855 Outrup');
-SELECT * FROM Employee;
+CREATE TABLE Vaccination (
+vaccination_ID		SMALLINT,
+vaccinated_dateTime DATETIME,
+CPR					BIGINT(11),
+vaccine_type		ENUM('covaxx','aspera','blast3000','divoc'),
+city				VARCHAR(20),
+emp_no				SMALLINT,
+PRIMARY KEY (vaccination_ID),
+FOREIGN KEY (CPR)
+	REFERENCES Customer(CPR),
+FOREIGN KEY (vaccine_type)
+	REFERENCES Vaccinetype(vaccine_type),
+FOREIGN KEY (city)
+	REFERENCES Occurs(city),
+FOREIGN KEY (emp_no)
+	REFERENCES Employee(emp_no)
+);
+
+
+
