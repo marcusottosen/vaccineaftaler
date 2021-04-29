ca433dce6f363ce37dc9a1f76d6ac987833771b1
rjdp123 <71651968+rjdp123@users.noreply.github.com>
2021-04-29 14:20:27 +0200

    Add files via upload
    Nyt Java wuhu

---
+++ Vaccine_java/src/main/java/dk/dtu/f21_02327/Importdata.java
@@ -0,0 +1,35 @@
+package dk.dtu.f21_02327;
+import java.sql.*;
+
+public class Importdata {
+    //Database connection
+    String HOST     = "localhost";
+    int PORT        = 3306;
+    String DATABASE  = "vaccineberedskabet";
+    String USERNAME = "root";
+    String PASSWORD = "Gce59hgh";
+
+    String url = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE + "?user=root";
+    public Connection connection;
+
+    {
+        try {
+            connection = DriverManager.getConnection(url, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
+    public void Importdata(long cprnr, String name, Date date, Time time, String vaccinetype, String location) throws SQLException {
+        PreparedStatement datainsert = connection.prepareStatement("INSERT INTO Appointment (daily_appointment_ID, CPR, appointment_name, appointment_date, appointment_time, vaccine_type, city) VALUES (?, ?, ?, ?, ?, ?, ?) ");
+        datainsert.setString(1,null);
+        datainsert.setLong(2, cprnr);
+        datainsert.setString(3, name);
+        datainsert.setDate(4, date);
+        datainsert.setTime(5, time);
+        datainsert.setString(6, vaccinetype);
+        datainsert.setString(7, location);
+       datainsert.executeUpdate();
+    }
+}
